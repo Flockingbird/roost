6df05dc0cb55a7e8b337f1d1c5833be496d31659
Bèr Kessels <ber@berk.es>
2020-11-06 16:13:01 +0100

    Document purpose of events

--- app/events/confirmation_email_sent.rb
+++ app/events/confirmation_email_sent.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
+##
+# A confirmation email has been sent
 ConfirmationEmailSent = Class.new(EventSourcery::Event)
--- app/events/registration_confirmed.rb
+++ app/events/registration_confirmed.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
+##
+# A Registration is confirmed
 RegistrationConfirmed = Class.new(EventSourcery::Event)
