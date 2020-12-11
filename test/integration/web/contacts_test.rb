fc390beb83d59b41e27384a0693df9455b04956e
Bèr Kessels <ber@berk.es>
2020-12-11 12:20:13 +0100

    Simplify integration test by moving common behaviour into before

--- test/integration/web/contacts_test.rb
+++ test/integration/web/contacts_test.rb
@@ -8,32 +8,27 @@ require 'test_helper'
 # And I click the "add to contacts" button
 # Then the member is added to my contacts
 class MemberAddsToContactsTest < Minitest::WebSpec
-  before { harry && ron }
+  before do
+    harry
+    ron
+
+    as(harry)
+    discover_member(username: ron[:username]).upto(:profile_visited)
+    adds_contact.upto(:contact_added)
+  end
 
   it 'adds another member to contacts' do
-    as(harry) do
-      # TODO: when the workflow supports actual discovery, use the
-      # visible name and not the username here.
-      discover_member(username: ron[:username]).upto(:profile_visited)
-      adds_contact.upto(:contact_added)
+    # NOTE that the handle uses .com and rons email .org
+    assert_content(
+      flash(:success),
+      'ron@example.com was added to your contacts'
+    )
 
-      # NOTE that the handle uses .com and rons email .org
-      assert_content(
-        flash(:success),
-        'ron@example.com was added to your contacts'
-      )
-
-      visit '/contacts'
-      assert_content('ron@example.com')
-    end
+    visit '/contacts'
+    assert_content('ron@example.com')
   end
 
   it 'gets a notification when someone added me as a contact' do
-    as(harry) do
-      discover_member(username: ron[:username]).upto(:profile_visited)
-      adds_contact.upto(:contact_added)
-    end
-
     as(ron) do
       main_menu('Updates').click
       assert_content "hpotter@example.com #{Date.today}"
